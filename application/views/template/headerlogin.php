65a41150609852ac015c0b7eada930c3337f4054
danielwijaw <daniel.wija.w@gmail.com>
2019-08-08 13:47:50 +0700

    management surat

--- application/views/template/headerlogin.php
+++ application/views/template/headerlogin.php
@@ -17,7 +17,7 @@
     <nav class="navbar navbar-static-top">
       <div class="container">
         <div class="navbar-header">
-          <a href="<?php echo base_url() ?>" class="navbar-brand"><b>Letter</b></a>
+          <a href="<?php echo base_url() ?>" class="navbar-brand"><b>Management Surat</b></a>
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
             <i class="fa fa-bars"></i>
           </button>
